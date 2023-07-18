eb2f69bb86802231b3a7d67086fdffc710cf67c5
Bunty <bunty@bonnys-mbp.wireless.concordia.ca>
2023-07-17 20:13:59 -0400

    OG-8 add post-job component

---
+++ HireHubFrontend/src/app/model/post-jobs.ts
@@ -0,0 +1,10 @@
+export class PostJobs {
+    id:number | undefined;
+    title : string = '';
+    skills : string = '';
+    role : string = '';
+    type : string = '';
+    experience : string='';
+    description : string= '';
+    postedBy:any;
+}
