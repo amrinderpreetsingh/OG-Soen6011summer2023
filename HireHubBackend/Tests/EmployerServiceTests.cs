4c7c1351be869d3dc3936de49f10ee2eb50eab64
Gaganpreet Singh <gagan2807@gmail.com>
2023-07-29 21:07:28 -0400

    OG-55 | add unit test cases

---
+++ HireHubBackend/Tests/EmployerServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogicLayer.Services;
+using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using Moq;
+using Xunit;
+
+namespace Test.ServiceTests
+{
+    public class EmployerServiceTests
+    {
+
+        [Theory]
+        [InlineData("jobs@kpmg.com", "1234")]
+        [InlineData("jobs@meta.com", "12345")]
+        [InlineData("JOBS@meta.com", "12345")]
+        public void EmployerLoginReturnsTrue(string email, string password)
+        {
+            var mockService = new Mock<IDatabaseDataService>();
+            var serviceReturn = new List<Employer>()
+            {
+                new Employer
+                {
+                    CompanyEmail="jobs@kpmg.com",
+                    CompanyPassword="1234"
+                },
+                new Employer
+                {
+                    CompanyEmail="jobs@meta.com",
+                    CompanyPassword="12345"
+                }
+            };
+            var c = new EmployerService(mockService.Object);
+            mockService.Setup(x => x.GetAllEmployers()).Returns(serviceReturn);
+            var result = c.Login(email, password);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("jobs@nagarro.com", "1234")]
+        [InlineData("jobs@tcs.com", "12345")]
+        public void EmployerLoginReturnsFalse(string email, string password)
+        {
+            var mockService = new Mock<IDatabaseDataService>();
+            var serviceReturn = new List<Employer>()
+            {
+                new Employer
+                {
+                    CompanyEmail="jobs@kpmg.com",
+                    CompanyPassword="1234"
+                },
+                new Employer
+                {
+                    CompanyEmail="jobs@kpmg.com",
+                    CompanyPassword="1234"
+                }
+            };
+            var c = new EmployerService(mockService.Object);
+            mockService.Setup(x => x.GetAllEmployers()).Returns(serviceReturn);
+            var result = c.Login(email, password);
+            Assert.False(result);
+        }
+    }
+}
---
+++ HireHubBackend/Tests/JobServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogicLayer.Services;
+using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using Moq;
+using Xunit;
+
+namespace Test.ServiceTests
+{
+    public class JobServiceTests
+    {
+
+        [Theory]
+        [InlineData("jobs@nagarro.com",2)]
+        [InlineData("jobs@kpmg.com",1)]
+        public void GetJobsPostedByAnEmployerIsOk(string email,int expectedCount)
+        {
+            var mockService = new Mock<IDatabaseDataService>();
+
+            var jobs = new List<Job>()
+            {
+                 new Job
+                {
+                    Title="Engineer",
+                    Id=1,
+                    Description="hardworker",
+                    Experience="2",
+                    Role="Full-Time",
+                    Skills="Java",
+                    PostedBy="jobs@nagarro.com",
+                    Type="Intern"
+                 },
+                 new Job
+                 {
+                     Title="Associate Engineer",
+                    Id=1,
+                    Description="hardworker",
+                    Experience="2",
+                    Role="Full-Time",
+                    Skills="Java",
+                    PostedBy="jobs@nagarro.com",
+                    Type="Intern"
+                 },
+                 new Job
+                 {
+                      Title="Associate Engineer",
+                    Id=1,
+                    Description="hardworker",
+                    Experience="2",
+                    Role="Full-Time",
+                    Skills="Java",
+                    PostedBy="jobs@kpmg.com",
+                    Type="Intern"
+                 }
+
+            };
+
+            mockService.Setup(x => x.GetAllJobs()).Returns(jobs);
+            var sut = new JobService(mockService.Object);
+            var result = sut.GetJobsPostedByAnEmployer(email);
+
+            Assert.Equal(expectedCount, result.Count);
+
+        }
+    }
+}
